252ad99c0dc745520a2df94c10c4267702e00d51
refrenc <ggim9718@gmail.com>
2023-11-13 12:10:55 +0900

    test: 특별 할인 Condition 테스트 코드

---
+++ src/test/java/christmas/domain/condition/SpecialDayConditionTest.java
@@ -0,0 +1,24 @@
+package christmas.domain.condition;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpecialDayConditionTest {
+
+    @Test
+    void 특별_할인_기간_테스트_참() {
+        assertThat(new SpecialDayCondition().isEventDay(25)).isTrue();
+    }
+
+    @Test
+    void 특별_할인_기간_테스트_거짓() {
+        assertThat(new SpecialDayCondition().isEventDay(1)).isFalse();
+    }
+
+    @Test
+    void 특별_할인_적용여부_테스트() {
+        assertThat(new SpecialDayCondition().isEventApplicable()).isTrue();
+    }
+}
