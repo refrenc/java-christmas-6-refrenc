93d40ad22026fedc7eb6f1229ff46d4bc05ae85d
refrenc <ggim9718@gmail.com>
2023-11-13 15:53:04 +0900

    feat: InputView 예외처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,14 +2,19 @@ package christmas.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuItem;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputView {
 
     static public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = readLine();
-        if (!verifyDate(input)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
+        verifyDate(input);
 
         return Integer.parseInt(input);
     }
@@ -17,20 +22,54 @@ public class InputView {
     static public String readOrder() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = readLine();
-        if (!verifyOrder(input)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
+        verifyOrder(input);
 
         return input;
     }
 
-    static boolean verifyDate(String input) {
+    static void verifyDate(String input) {
         // TODO: 1 이상 31 이하의 숫자
-        return false;
+        try {
+            Integer date = Integer.parseInt(input);
+            if (date < 1 || date > 31) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
-    static boolean verifyOrder(String input) {
+    static void verifyOrder(String input) {
         // TODO: {메뉴}-{개수}의 형식, 개수는 1 이상의 숫자, 메뉴 중복 불가, 존재하는 메뉴만 가능
-        return false;
+        String[] orders = input.split(",");
+        List<MenuItem> menus = new ArrayList<>();
+        Set<Menu> menuTypes = new HashSet<>();
+        for (String order : orders) {
+            String[] fragment = order.split("-");
+            String menu = fragment[0];
+            if (fragment.length != 2
+                    || menus.contains(MenuItem.fromName(menu))
+                    || convertStringToInteger(fragment[1]) < 1) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+            menus.add(MenuItem.fromName(menu));
+            menuTypes.add(Menu.fromName(menu));
+        }
+
+        isBeverageOnly(menuTypes);
+    }
+
+    static private Integer convertStringToInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    static private void isBeverageOnly(Set<Menu> menus) {
+        if (menus.size() == 1 && menus.contains(Menu.BEVERAGE)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
--- src/test/java/christmas/view/InputViewTest.java
+++ src/test/java/christmas/view/InputViewTest.java
@@ -3,6 +3,7 @@ package christmas.view;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
@@ -23,23 +24,49 @@ class InputViewTest {
 
     @Test
     void 방문_날짜_테스트() {
-        assertThat(inputView.verifyDate("25")).isTrue();
+        try {
+            inputView.verifyDate("25");
+        } catch (IllegalArgumentException e) {
+            Assertions.fail("");
+        }
     }
 
     @Test
-    void 주문_예외_테스트() {
-        assertThatThrownBy(() -> inputView.verifyOrder("메뉴"))
+    void 주문_형식_예외_테스트() {
+        assertThatThrownBy(() -> inputView.verifyOrder("타파스"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 주문_없는_메뉴_예외_테스트() {
+        assertThatThrownBy(() -> inputView.verifyOrder("메뉴-2"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 주문_개수_예외_테스트() {
-        assertThatThrownBy(() -> inputView.verifyOrder("메뉴-0"))
+        assertThatThrownBy(() -> inputView.verifyOrder("타파스-0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 주문_중복_예외_테스트() {
+        assertThatThrownBy(() -> inputView.verifyOrder("타파스-2,타파스-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 주문_음료만_예외_테스트() {
+        assertThatThrownBy(() -> inputView.verifyOrder("샴페인-1,제로콜라-1"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 주문_테스트() {
-        assertThat(inputView.verifyDate("메뉴-3")).isTrue();
+        try {
+            inputView.verifyOrder("샴페인-3,타파스-2");
+        } catch (IllegalArgumentException e) {
+            Assertions.fail("");
+        }
     }
 }
