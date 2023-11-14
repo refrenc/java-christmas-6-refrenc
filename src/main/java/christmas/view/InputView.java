89d685682f01ba393252c9b52d28c7877fbd5567
refrenc <ggim9718@gmail.com>
2023-11-14 16:56:34 +0900

    refactor: InputView 주문 input 검사 리팩토링

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -42,21 +42,38 @@ public class InputView {
     static void verifyOrder(String input) {
         // TODO: {메뉴}-{개수}의 형식, 개수는 1 이상의 숫자, 메뉴 중복 불가, 존재하는 메뉴만 가능, 음료만 주문 불가
         String[] orders = input.split(",");
-        List<MenuItem> menus = new ArrayList<>();
-        Set<Menu> menuTypes = new HashSet<>();
+        if (orders.length > 20
+                || !validateOrderFormat(orders)
+                || hasDuplicatedMenu(orders)
+                || isBeverageOnly(orders)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    static private boolean validateOrderFormat(String[] orders) {
         for (String order : orders) {
             String[] fragment = order.split("-");
             String menu = fragment[0];
-            if (orders.length > 20 || fragment.length != 2
-                    || menus.contains(MenuItem.fromName(menu))
-                    || convertStringToInteger(fragment[1]) < 1) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            if (fragment.length != 2 || convertStringToInteger(fragment[1]) < 1) {
+                return false;
             }
-            menus.add(MenuItem.fromName(menu));
-            menuTypes.add(Menu.fromName(menu));
         }
 
-        isBeverageOnly(menuTypes);
+        return true;
+    }
+
+    static private boolean hasDuplicatedMenu(String[] orders) {
+        List<MenuItem> menus = new ArrayList<>();
+        for (String order : orders) {
+            String[] fragment = order.split("-");
+            String menu = fragment[0];
+            if (menus.contains(MenuItem.fromName(menu))) {
+                return true;
+            }
+            menus.add(MenuItem.fromName(menu));
+        }
+
+        return false;
     }
 
     static private Integer convertStringToInteger(String input) {
@@ -67,9 +84,18 @@ public class InputView {
         }
     }
 
-    static private void isBeverageOnly(Set<Menu> menus) {
-        if (menus.size() == 1 && menus.contains(Menu.BEVERAGE)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    static private boolean isBeverageOnly(String[] orders) {
+        Set<Menu> menuTypes = new HashSet<>();
+        for (String order : orders) {
+            String[] fragment = order.split("-");
+            String menu = fragment[0];
+            menuTypes.add(Menu.fromName(menu));
         }
+
+        if (menuTypes.size() == 1 && menuTypes.contains(Menu.BEVERAGE)) {
+            return true;
+        }
+
+        return false;
     }
 }
